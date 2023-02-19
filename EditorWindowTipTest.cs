a575cf989a7a4850eb7d45ce8d8df7f1a2d13dca
Hunterson Studio <68167854+HuntersonStudio@users.noreply.github.com>
2023-02-19 16:18:44 +0100

    Update EditorWindowTipTest.cs

--- EditorWindowTipTest.cs
+++ EditorWindowTipTest.cs
@@ -1,12 +1,14 @@
 <div class="horizontal">
-   <script src="https://emgithub.com/embed-v2.js?target=https%3A%2F%2Fgithub.com%2FHuntersonStudio%2FExamples%2Fblob%2Fmain%2FEditorWindowTipTest.cs%23L7-L18&style=github-dark-dimmed&type=code"></script>
+   <script src="https://emgithub.com/embed-v2.js?target=https%3A%2F%2Fgithub.com%2FHuntersonStudio%2FExamples%2Fblob%2Fmain%2FEditorWindowTipTest.cs%23{sample1}&style=github-dark-dimmed&type=code"></script>
 </div>
 
 <div class="horizontal paragraph">
    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum facilisis placerat. Fusce auctor elit erat, in pharetra est vehicula eget. Nam arcu nunc, feugiat eget imperdiet in, facilisis eu ligula. Aliquam vel auctor tortor. Vivamus nunc erat, tristique at dolor eget, sodales feugiat augue. Mauris vel sem in sem egestas luctus ut eget eros. Curabitur quis eleifend turpis. Donec euismod, sapien a sagittis accumsan, sapien augue sagittis sapien, id sollicitudin orci libero sit amet purus. Suspendisse potenti. Mauris scelerisque magna ante, dictum cursus nisi eleifend non.</p>
 </div>
-#end
 
+#example
+
+#begin sample1
 public unsafe void Add(int _int, int _index = -1)
 {
     int index = _index <= -1 ? tracker : _index;
@@ -19,3 +21,4 @@ public unsafe void Add(int _int, int _index = -1)
     if (_index <= -1)
         tracker += 4;
 }
+#end sample1
