96c2b4450be5c9647da5cfbb90c16dac15727acc
Hunterson Studio <68167854+HuntersonStudio@users.noreply.github.com>
2023-02-28 22:25:20 +0100

    Update EditorWindowTipTest.cs

--- EditorWindowTipTest.cs
+++ EditorWindowTipTest.cs
@@ -1,9 +1,6 @@
-#code sample1
-
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum facilisis placerat. Fusce auctor elit erat, in pharetra est vehicula eget. Nam arcu nunc, feugiat eget imperdiet in, facilisis eu ligula. Aliquam vel auctor tortor. Vivamus nunc erat, tristique at dolor eget, sodales feugiat augue. Mauris vel sem in sem egestas luctus ut eget eros. Curabitur quis eleifend turpis. Donec euismod, sapien a sagittis accumsan, sapien augue sagittis sapien, id sollicitudin orci libero sit amet purus. Suspendisse potenti. Mauris scelerisque magna ante, dictum cursus nisi eleifend non.
-#example
 
-#begin sample1
+#begin-code
 public unsafe void Add(int _int, int _index = -1)
 {
     int index = _index <= -1 ? tracker : _index;
@@ -16,4 +13,6 @@ public unsafe void Add(int _int, int _index = -1)
     if (_index <= -1)
         tracker += 4;
 }
-#end sample1
+#end-code
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum facilisis placerat. Fusce auctor elit erat, in pharetra est vehicula eget. Nam arcu nunc, feugiat eget imperdiet in, facilisis eu ligula. Aliquam vel auctor tortor. Vivamus nunc erat, tristique at dolor eget, sodales feugiat augue. Mauris vel sem in sem egestas luctus ut eget eros. Curabitur quis eleifend turpis. Donec euismod, sapien a sagittis accumsan, sapien augue sagittis sapien, id sollicitudin orci libero sit amet purus. Suspendisse potenti. Mauris scelerisque magna ante, dictum cursus nisi eleifend non.
